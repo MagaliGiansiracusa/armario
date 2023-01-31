f10d16692191d56cdc9ad9db29d041ddb959500c
MagaliGiansiracusa <giansiracusamagali@gmail.com>
2023-01-30 21:39:37 -0300

    Update Header.jsx

--- frontEnd/components/layout/Header.jsx
+++ frontEnd/components/layout/Header.jsx
@@ -1,21 +1,34 @@
 import styles from "./Header.module.scss";
 import Link from "next/link";
+import React from 'react'
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
     return (
         <div className={styles["header"]}>
-            <nav className={styles["navbar"]}>
-                <h2>Wardrobe</h2>
-                <ul>
-                    <li>
-                        <Link href="/">Mi armario</Link>
-                    </li>
-                    <li>
-                        <Link href="/add">Nueva Prenda</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                        <div className={styles["respmenu"]} >
+                                            
+                        
+                            <nav className={styles["navbar"]}>
+                            <input type="checkbox" className ={styles["checkbox"]}/>
+                            <FaBars className={styles["burger"]}/>
+                                <ul className={styles["links"]}>
+                                    <li>
+                                        <Link href={"/historia"}>Historia</Link>
+                                    </li>
+                                    <li>
+                                        <Link href={"/recetas"} >Recetas</Link>
+                                    </li>
+                                    <li>
+                                        <Link href={"/"} >Home</Link>
+                                    </li>
+            
+                                </ul>
+                            </nav>
+                            </div>
+                        
+                    </div>
+        
     );
 };
 
